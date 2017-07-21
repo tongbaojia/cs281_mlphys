510b3548934365c84083a41948e60544982789e6
Jonah Philion <jonahphilion@heplmac7-2.local>
2017-07-20 22:53:33 -0400

    new feats

--- visual.py
+++ visual.py
@@ -9,6 +9,7 @@ from sklearn.neighbors import NearestNeighbors
 from collections import defaultdict
 from tqdm import tqdm
 import xgboost
+import itertools
 
 print 'started'
 t0 = time()
@@ -16,21 +17,34 @@ df = pd.read_csv('../dark_matter_data/train.csv', index_col=0)
 t1 = time()
 print 'time to load', t1-t0
 
-df_play = df[:500000]
+df_play = df[:1000000]
 
-# info_by_board = defaultdict(list)
-# for da in tqdm(df_play.values):
-# 	info_by_board[da[-1]].append(da[:-1])
+info_by_board = defaultdict(list)
+for da in tqdm(df_play.values):
+	info_by_board[da[-1]].append(da[:-1])
 
-# for k in info_by_board:
-# 	data = np.array(info_by_board[k])
-# 	nbrs = NearestNeighbors(n_neighbors = 5, algorithm='ball_tree')
-# 	nbrs.fit(df_play[:,1:4])
+all_X = []; all_Y = [];
+for k in tqdm(info_by_board):
+	data = np.array(info_by_board[k])
+	nbrs = NearestNeighbors(n_neighbors = 5, algorithm='ball_tree')
+	nbrs.fit(data[:,1:4])
+	for ix,indices in enumerate(nbrs.kneighbors(data[:,1:4])[1]):
+		sum_of_chi2 = sum(data[indices][:,-2])
+		position_vel = data[ix][1:7]
+		x_vec = np.append(position_vel, sum_of_chi2)
+		all_X.append(x_vec)
+		all_Y.append(data[ix][-1])
+all_X = np.array(all_X)
+all_Y = np.array(all_Y)
 
 
-train_X, test_X, train_Y, test_Y = train_test_split(df_play.values[:,1:7], df_play.values[:,7], test_size=.3, random_state=35)
+train_X, test_X, train_Y, test_Y = train_test_split(all_X, all_Y, test_size=.3, random_state=35)
 
-model = xgboost.XGBClassifier(max_depth=5, min_child_weight=1, learning_rate=.1, gamma=0, subsample=.8,
+# max_depth_arr = [3,5,7,9]
+# min_child_weight_arr = [3,5,7,9]
+# gamma_arr = [0.0,.1,.2,.3,.4]
+#for gamma in gamma_arr:
+model = xgboost.XGBClassifier(max_depth=9, min_child_weight=5, learning_rate=.1, gamma=0, subsample=.8,
 	colsample_bytree=.8, scale_pos_weight = 1)
 #model = RandomForestClassifier(n_estimators=128, verbose=1, n_jobs = -1)
 print 'fitting'
@@ -45,6 +59,7 @@ test_score = roc_auc_score(test_Y, preds_test)
 
 print 'train score', train_score
 print 'test_score', test_score
+print ''
 
 
 print 'started'
@@ -58,7 +73,7 @@ preds_submit = model.predict_proba(X_submit)[:,1]
 
 submission = pd.DataFrame(preds_submit, columns=['Prediction'])
 submission.index.name = 'Id'
-submission.to_csv('xgblocal3.csv', header=True)
+submission.to_csv('xgblocal4.csv', header=True)
 
 
 
